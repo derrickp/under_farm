81b982ea35a5ffd6b432c53496435149ba02d45e
Derrick Plotsky <derrickplotsky@gmail.com>
2021-06-21 18:49:49 -0700

    Add bucket for our sprite handles
    This has actually gone over a few iterations, and I'm very sure it will
    go over more as I start adding things like animations.
    
    But right now, this is just a container for the different handles we'll
    need when referencing textures.

---
+++ src/sprite_handles.rs
@@ -0,0 +1,16 @@
+use bevy::{
+    prelude::{Handle, HandleUntyped, Texture},
+    sprite::TextureAtlas,
+};
+
+#[derive(Default)]
+pub struct Sprites {
+    pub atlas_handle: Handle<TextureAtlas>,
+    pub player_sprite_handle: Handle<Texture>,
+    pub background_handle: Handle<Texture>,
+}
+
+#[derive(Default)]
+pub struct LoadedTextures {
+    pub handles: Vec<HandleUntyped>,
+}
