0f2ee7b8ea1d237b4c9aafb77f5bf86ad6cc42bf
Derrick Plotsky <derrickplotsky@gmail.com>
2021-06-21 18:52:58 -0700

    Bring all of the pieces together
    This does it all. Loads up all the systems as system sets, which gives
    us some nice control of the systems that go in and are running during
    different app states.
    
    A lot for the very very basic thing this does which is draw a sprite to
    the screen.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+mod app_state;
+mod components;
+mod sprite_handles;
+mod systems;
+
+use app_state::AppState;
+use bevy::prelude::*;
+use sprite_handles::{LoadedTextures, Sprites};
+use systems::{
+    initial_spawns::spawn_opening_bundles,
+    textures::{check_textures, load_sprites, load_textures},
+};
+
 fn main() {
-    println!("Hello, world!");
+    App::build()
+        .init_resource::<Sprites>()
+        .init_resource::<LoadedTextures>()
+        .add_state(AppState::Setup)
+        .add_plugins(DefaultPlugins)
+        .add_system_set(SystemSet::on_enter(AppState::Setup).with_system(load_textures.system()))
+        .add_system_set(SystemSet::on_update(AppState::Setup).with_system(check_textures.system()))
+        .add_system_set(SystemSet::on_enter(AppState::Finished).with_system(load_sprites.system()))
+        .add_system_set(
+            SystemSet::on_enter(AppState::Playing).with_system(spawn_opening_bundles.system()),
+        )
+        .run();
 }
