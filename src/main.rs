3ec0ca1e1886c8d4abe27523da48a54e93313080
Derrick Plotsky <derrickplotsky@gmail.com>
2021-06-30 23:25:52 -0700

    Use new systems and GameState
    This brings every together.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
 mod app_state;
 mod components;
+mod game_state;
 mod sprite_handles;
 mod systems;
 
 use app_state::AppState;
 use bevy::{input::system::exit_on_esc_system, prelude::*};
+use bevy_tilemap::prelude::TilemapDefaultPlugins;
+use game_state::GameState;
 use sprite_handles::{LoadedTextures, Sprites};
 use systems::{
-    initial_spawns::spawn_opening_bundles,
+    initial_spawns::{build_random_dungeon, spawn_opening_bundles},
     inputs::keyboard_input_system,
     player_movement::calculate_movement,
     textures::{check_textures, load_sprites, load_textures},
@@ -16,14 +19,20 @@ use systems::{
 fn main() {
     App::build()
         .init_resource::<Sprites>()
+        .init_resource::<GameState>()
         .init_resource::<LoadedTextures>()
         .add_state(AppState::Setup)
         .add_plugins(DefaultPlugins)
+        .add_plugins(TilemapDefaultPlugins)
         .add_system_set(SystemSet::on_enter(AppState::Setup).with_system(load_textures.system()))
         .add_system_set(SystemSet::on_update(AppState::Setup).with_system(check_textures.system()))
-        .add_system_set(SystemSet::on_enter(AppState::TexturesLoaded).with_system(load_sprites.system()))
         .add_system_set(
-            SystemSet::on_enter(AppState::SpritesLoaded).with_system(spawn_opening_bundles.system()),
+            SystemSet::on_enter(AppState::TexturesLoaded).with_system(load_sprites.system()),
+        )
+        .add_system_set(
+            SystemSet::on_enter(AppState::SpritesLoaded)
+                .with_system(spawn_opening_bundles.system())
+                .with_system(build_random_dungeon.system()),
         )
         .add_system_set(
             SystemSet::on_update(AppState::Playing)
