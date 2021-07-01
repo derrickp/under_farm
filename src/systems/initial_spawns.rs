01ddecfc4987742c4787c4da5d4c786fcf64f6d5
Derrick Plotsky <derrickplotsky@gmail.com>
2021-06-30 23:23:34 -0700

    Add systems for spawning and creating tilemap
    I don't necessarily love the way this is split up, but essentially I
    create the tilemap once the sprites are loaded, and then in my spawning
    code I actually build the map by just creating a whole bunch of tiles up
    front.
    
    Later I might add code to move the camera and let the auto-spawning of
    bevy_tiled take over.

--- src/systems/initial_spawns.rs
+++ src/systems/initial_spawns.rs
@@ -1,9 +1,27 @@
-use bevy::{math::Vec3, prelude::{Assets, Commands, OrthographicCameraBundle, Res, ResMut, SpriteSheetBundle, State, Transform}, sprite::{TextureAtlas, TextureAtlasSprite}};
+use bevy::{
+    math::Vec3,
+    prelude::{
+        AssetServer, Assets, Commands, Handle, Query, Res, ResMut, SpriteSheetBundle, State,
+        Texture, Transform,
+    },
+    sprite::{TextureAtlas, TextureAtlasSprite},
+};
+use bevy_tilemap::{Tile, Tilemap};
 
-use crate::{app_state::AppState, components::{
+use crate::{
+    app_state::AppState,
+    components::{
         player::{Player, PlayerBundle, PlayerName},
         speed::Speed,
-    }, sprite_handles::Sprites};
+    },
+    game_state::GameState,
+    sprite_handles::Sprites,
+};
+
+const CHUNK_WIDTH: u32 = 24;
+const CHUNK_HEIGHT: u32 = 24;
+const TILEMAP_WIDTH: i32 = CHUNK_WIDTH as i32 * 48;
+const TILEMAP_HEIGHT: i32 = CHUNK_HEIGHT as i32 * 48;
 
 pub fn spawn_opening_bundles(
     mut commands: Commands,
@@ -12,37 +30,19 @@ pub fn spawn_opening_bundles(
     mut state: ResMut<State<AppState>>,
 ) {
     let texture_atlas = texture_atlases.get(&sprite_handles.atlas_handle).unwrap();
-    let background_index = texture_atlas
-        .get_texture_index(&sprite_handles.background_handle)
-        .unwrap();
     let sprite_index = texture_atlas
         .get_texture_index(&sprite_handles.player_sprite_handle)
         .unwrap();
-    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
-
-    commands.spawn_bundle(SpriteSheetBundle {
-        transform: Transform {
-            translation: Vec3::new(150.0, 0.0, 0.0),
-            scale: Vec3::splat(4.0),
-            ..Default::default()
-        },
-        sprite: TextureAtlasSprite::new(background_index as u32),
-        texture_atlas: sprite_handles.atlas_handle.clone(),
-        ..Default::default()
-    });
 
     commands.spawn_bundle(PlayerBundle {
         name: PlayerName("Goblin?".to_string()),
-        speed: Speed {
-            x: 0,
-            y: 0,
-        },
+        speed: Speed { x: 0, y: 0 },
         sprite: SpriteSheetBundle {
             sprite: TextureAtlasSprite::new(sprite_index as u32),
             texture_atlas: sprite_handles.atlas_handle.clone(),
             transform: Transform {
-                translation: Vec3::new(0.0, 0.0, 0.0),
-                scale: Vec3::splat(2.0),
+                translation: Vec3::new(0.0, 0.0, 5.0),
+                scale: Vec3::splat(1.0),
                 ..Default::default()
             },
             ..Default::default()
@@ -51,4 +51,45 @@ pub fn spawn_opening_bundles(
     });
 
     state.set(AppState::Playing).unwrap(); // For now
+}
+
+pub fn build_random_dungeon(
+    mut game_state: ResMut<GameState>,
+    texture_atlases: Res<Assets<TextureAtlas>>,
+    asset_server: Res<AssetServer>,
+    mut query: Query<&mut Tilemap>,
+) {
+    if game_state.map_loaded {
+        return;
+    }
+
+    for mut map in query.iter_mut() {
+        // Then we need to find out what the handles were to our textures we are going to use.
+        let floor_sprite: Handle<Texture> = asset_server.get_handle("sprites/background.png");
+        let texture_atlas = texture_atlases.get(map.texture_atlas()).unwrap();
+        let floor_idx = texture_atlas.get_texture_index(&floor_sprite).unwrap();
+
+        // Now we fill the entire space with floors.
+        let mut tiles = Vec::new();
+        for y in 0..TILEMAP_HEIGHT {
+            for x in 0..TILEMAP_WIDTH {
+                let y = y - TILEMAP_HEIGHT / 2;
+                let x = x - TILEMAP_WIDTH / 2;
+                // By default tile sets the Z order at 0. Lower means that tile
+                // will render lower than others. 0 is the absolute bottom
+                // level which is perfect for backgrounds.
+                let tile = Tile {
+                    point: (x, y),
+                    sprite_index: floor_idx,
+                    ..Default::default()
+                };
+                tiles.push(tile);
+            }
+        }
+
+        // Now we pass all the tiles to our map.
+        map.insert_tiles(tiles).unwrap();
+
+        game_state.map_loaded = true;
+    }
 }
--- src/systems/textures.rs
+++ src/systems/textures.rs
@@ -1,11 +1,25 @@
 use bevy::{
     asset::LoadState,
-    prelude::{AssetServer, Assets, Res, ResMut, State, Texture},
+    core::Timer,
+    prelude::{
+        AssetServer, Assets, Commands, OrthographicCameraBundle, Res, ResMut, State, Texture,
+        Visible,
+    },
     sprite::{TextureAtlas, TextureAtlasBuilder},
 };
+use bevy_tilemap::{
+    prelude::{LayerKind, TilemapBundle},
+    Tilemap, TilemapLayer,
+};
+
+const CHUNK_WIDTH: u32 = 24;
+const CHUNK_HEIGHT: u32 = 24;
+const TILEMAP_WIDTH: i32 = CHUNK_WIDTH as i32 * 48;
+const TILEMAP_HEIGHT: i32 = CHUNK_HEIGHT as i32 * 48;
 
 use crate::{
     app_state::AppState,
+    game_state::GameState,
     sprite_handles::{LoadedTextures, Sprites},
 };
 
@@ -32,7 +46,10 @@ pub fn load_sprites(
     mut textures: ResMut<Assets<Texture>>,
     mut state: ResMut<State<AppState>>,
     asset_server: Res<AssetServer>,
+    mut commands: Commands,
+    mut game_state: ResMut<GameState>,
 ) {
+    game_state.map_loaded = false;
     let mut texture_atlas_builder = TextureAtlasBuilder::default();
     for handle in loaded_textures.handles.iter() {
         let texture = textures.get(handle).unwrap();
@@ -41,13 +58,45 @@ pub fn load_sprites(
 
     let texture_atlas = texture_atlas_builder.finish(&mut textures).unwrap();
     let atlas_handle = texture_atlases.add(texture_atlas);
-    sprite_handles.atlas_handle = atlas_handle;
+    sprite_handles.atlas_handle = atlas_handle.clone();
 
     let texture_handle = asset_server.load("sprites/goblin_big_hat.png");
     sprite_handles.player_sprite_handle = texture_handle;
 
     let background_handle = asset_server.get_handle("sprites/background.png");
     sprite_handles.background_handle = background_handle;
+
+    let tilemap = Tilemap::builder()
+        .dimensions(TILEMAP_WIDTH as u32, TILEMAP_HEIGHT as u32)
+        .chunk_dimensions(CHUNK_WIDTH, CHUNK_HEIGHT, 1)
+        .texture_dimensions(24, 24)
+        .auto_chunk()
+        .auto_spawn(2, 2)
+        .add_layer(
+            TilemapLayer {
+                kind: LayerKind::Dense,
+                ..Default::default()
+            },
+            0,
+        )
+        .texture_atlas(atlas_handle.clone())
+        .finish()
+        .unwrap();
+
+    let tilemap_components = TilemapBundle {
+        tilemap,
+        visible: Visible {
+            is_visible: true,
+            is_transparent: true,
+        },
+        transform: Default::default(),
+        global_transform: Default::default(),
+    };
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+    commands
+        .spawn()
+        .insert_bundle(tilemap_components)
+        .insert(Timer::from_seconds(0.075, true));
 
     state.set(AppState::SpritesLoaded).unwrap();
 }
