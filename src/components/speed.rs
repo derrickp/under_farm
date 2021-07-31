b1b9b10e42eef825542141e10c34c50d66d3ee2f
Derrick Plotsky <derrickplotsky@gmail.com>
2021-07-30 22:43:59 -0700

    Add a default for Speed
    Standing still is the default.

--- src/components/speed.rs
+++ src/components/speed.rs
@@ -3,3 +3,11 @@ use bevy::math::Vec2;
 pub struct Speed {
     pub current: Vec2,
 }
+
+impl Default for Speed {
+    fn default() -> Self {
+        return Speed {
+            current: Vec2::new(0.0, 0.0),
+        };
+    }
+}
