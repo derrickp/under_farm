3c0791819ad699c2ac48eb9685115162de18a235
Derrick Plotsky <derrickplotsky@gmail.com>
2021-08-12 20:29:45 -0700

    Enlarge our map
    MORE ROOM TO ROAM

--- src/configuration/map.rs
+++ src/configuration/map.rs
@@ -1,5 +1,5 @@
 use super::sprites::{SPRITE_SCALE, SPRITE_SIZE};
 
 pub const TILE_SIZE: f32 = SPRITE_SIZE * SPRITE_SCALE;
-pub const MAP_WIDTH: i32 = 50;
-pub const MAP_HEIGHT: i32 = 50;
+pub const MAP_WIDTH: i32 = 100;
+pub const MAP_HEIGHT: i32 = 100;
