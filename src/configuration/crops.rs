51f07caa6dbe30a2d4b5ca5e6ad655c5413113f1
Derrick Plotsky <derrickplotsky@gmail.com>
2021-08-12 18:26:09 -0700

    Update our crop configuration
    This updates all of our configuration to have configuration for the new
    stages and how long each crop is in each stage.

--- src/configuration/crops.rs
+++ src/configuration/crops.rs
@@ -18,47 +18,95 @@ pub struct CropStage {
     pub name: &'static str,
     pub sprite_location: &'static str,
     pub sprite_index: Option<u32>,
+    pub min_ticks_in_stage: u32,
+    pub chance_to_advance: u32,
 }
 
 pub struct CropConfigurations {
     pub configurations: Vec<CropConfiguration>,
 }
 
+const TICKS_PER_SECOND: u32 = 5;
+
 fn kane_configuration() -> CropConfiguration {
-    let stages = vec![CropStage {
-        name: "Kane Stalk",
-        sprite_location: "sprites/kane_stalks.png",
-        sprite_index: None,
-    }];
+    let stages = vec![
+        CropStage {
+            name: "Kane Seed",
+            sprite_location: "sprites/kane_seeds.png",
+            sprite_index: None,
+            min_ticks_in_stage: TICKS_PER_SECOND * 15,
+            chance_to_advance: 95,
+        },
+        CropStage {
+            name: "Kane Stalk",
+            sprite_location: "sprites/kane_stalks.png",
+            sprite_index: None,
+            min_ticks_in_stage: TICKS_PER_SECOND * 15,
+            chance_to_advance: 10,
+        },
+    ];
 
     return CropConfiguration::build_basic("Kane", stages);
 }
 
 fn mushroom_configuration() -> CropConfiguration {
-    let stages = vec![CropStage {
-        name: "Mushroom Plant",
-        sprite_location: "sprites/giant_mushroom.png",
-        sprite_index: None,
-    }];
+    let stages = vec![
+        CropStage {
+            name: "Mushroom Spores",
+            sprite_location: "sprites/giant_mushroom_spores.png",
+            sprite_index: None,
+            min_ticks_in_stage: TICKS_PER_SECOND * 10,
+            chance_to_advance: 95,
+        },
+        CropStage {
+            name: "Mushroom Plant",
+            sprite_location: "sprites/giant_mushroom.png",
+            sprite_index: None,
+            min_ticks_in_stage: TICKS_PER_SECOND * 10,
+            chance_to_advance: 5,
+        },
+    ];
     return CropConfiguration::build_basic("Mushroom", stages);
 }
 
 fn potato_configuration() -> CropConfiguration {
-    let stages = vec![CropStage {
-        name: "Potatoes",
-        sprite_location: "sprites/potatoes.png",
-        sprite_index: None,
-    }];
+    let stages = vec![
+        CropStage {
+            name: "Potato Seeds",
+            sprite_location: "sprites/potato_seeds.png",
+            sprite_index: None,
+            min_ticks_in_stage: TICKS_PER_SECOND * 5,
+            chance_to_advance: 95,
+        },
+        CropStage {
+            name: "Potatoes",
+            sprite_location: "sprites/potatoes.png",
+            sprite_index: None,
+            min_ticks_in_stage: TICKS_PER_SECOND * 5,
+            chance_to_advance: 5,
+        },
+    ];
 
     return CropConfiguration::build_basic("Potato", stages);
 }
 
 fn turnip_configuration() -> CropConfiguration {
-    let stages = vec![CropStage {
-        name: "Turnip",
-        sprite_location: "sprites/turnip_top.png",
-        sprite_index: None,
-    }];
+    let stages = vec![
+        CropStage {
+            name: "Turnip Seeds",
+            sprite_location: "sprites/turnip_seed.png",
+            sprite_index: None,
+            min_ticks_in_stage: TICKS_PER_SECOND * 20,
+            chance_to_advance: 75,
+        },
+        CropStage {
+            name: "Turnip",
+            sprite_location: "sprites/turnip_top.png",
+            sprite_index: None,
+            min_ticks_in_stage: TICKS_PER_SECOND * 25,
+            chance_to_advance: 25,
+        },
+    ];
 
     return CropConfiguration::build_basic("Turnip", stages);
 }
