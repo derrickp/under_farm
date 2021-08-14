9a3b4faef900af11264fda2f14ebbc435ffa4951
Derrick Plotsky <derrickplotsky@gmail.com>
2021-08-13 20:39:43 -0700

    Config for dirt floor sprite names
    These are my dirt floor sprites. I'll likely add more, but these will
    be the main floor in most of the cave.

--- src/configuration/sprites.rs
+++ src/configuration/sprites.rs
@@ -6,3 +6,11 @@ pub const SPRITE_SIZE: f32 = 32.0;
 pub fn sprite_scale() -> Vec3 {
     return Vec3::splat(SPRITE_SCALE);
 }
+
+pub fn dirt_floor_sprite_names() -> Vec<&'static str> {
+    return vec![
+        "sprites/dirt_floor_1.png",
+        "sprites/dirt_floor_2.png",
+        "sprites/dirt_floor_3.png",
+    ];
+}
