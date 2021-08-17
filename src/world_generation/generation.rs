3e8394654213ae061681b62fd76db2340f3b21af
Derrick Plotsky <derrickplotsky@gmail.com>
2021-08-16 23:48:53 -0700

    Spawn multiple rooms
    Attempt to spawn multiple rooms. Now the world generation attempts to
    put in 20 rooms. I don't attempt to add more if I can't get the full
    20, and that's fine.

--- src/world_generation/generation.rs
+++ src/world_generation/generation.rs
@@ -12,13 +12,27 @@ pub fn generate_world_grid() -> Grid {
 
     let room_sizes = room_sizes(RoomSize::TwoByTwo);
     let mut rng = rand::thread_rng();
-    let x: i32 = rng.gen_range(0..=(100 - room_sizes.0));
-    let y: i32 = rng.gen_range(0..=(100 - room_sizes.1));
 
-    println!("{} {}", x, y);
+    let mut room_count = 0;
 
-    let room = Room::two_by_two_square(x, y);
-    grid.add_room(room);
+    for _ in 0..20 {
+        let x: i32 = rng.gen_range(0..=(100 - room_sizes.0));
+        let y: i32 = rng.gen_range(0..=(100 - room_sizes.1));
+        let room = Room::two_by_two_square(x, y);
+
+        if room
+            .wall_coordinates
+            .iter()
+            .all(|coordinate| grid.is_cell_empty(coordinate.0, coordinate.1))
+        {
+            println!("{} {}", x, y);
+            room_count += 1;
+            grid.add_room(room);
+        }
+    }
+
+    println!("Room Count: {}", room_count);
+
     grid.fill_empty_cells();
     grid.create_outer_wall();
 
