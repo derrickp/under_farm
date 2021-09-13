62d7669945340583b782d8a587ee094113d149af
Derrick Plotsky <derrickplotsky@gmail.com>
2021-09-13 14:56:51 -0700

    Update our level generation
    Bigger level and more rooms.

--- src/systems/world.rs
+++ src/systems/world.rs
@@ -6,7 +6,8 @@ use tdlg::generator::Generator;
 
 use crate::{states::GameLoadState, world::WorldTickTimer};
 
-const DEFAULT_GRID_SIZE: usize = 100;
+const DEFAULT_GRID_SIZE: usize = 150;
+const NUMBER_OF_ROOMS: usize = 75;
 
 pub fn tick_game_world(time: Res<Time>, mut timer: ResMut<WorldTickTimer>) {
     timer.0.tick(time.delta());
@@ -15,10 +16,11 @@ pub fn tick_game_world(time: Res<Time>, mut timer: ResMut<WorldTickTimer>) {
 pub fn generate_world_grid(mut commands: Commands, mut load_state: ResMut<GameLoadState>) {
     let generator = Generator {
         grid_size: DEFAULT_GRID_SIZE,
-        target_number_rooms: 50,
+        target_number_rooms: NUMBER_OF_ROOMS,
         room_template_directory: "assets/room_templates",
     };
     let world = generator.generate_top_down_map();
+    println!("{}", world.room_count);
     commands.insert_resource(world.grid);
 
     load_state.game_world_generated = true;
