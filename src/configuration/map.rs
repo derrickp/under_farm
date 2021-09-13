fea503cd695455ae131942ef19397a65021602d4
Derrick Plotsky <derrickplotsky@gmail.com>
2021-09-12 22:11:23 -0700

    Stop returning Coordinate from translation
    The Coordinate struct I'm using in the crate doesn't work with f32
    however bevy already has something that covers this which is the
    Vec2.

--- src/configuration/map.rs
+++ src/configuration/map.rs
@@ -1,3 +1,4 @@
+use bevy::math::Vec2;
 use tdlg::coordinate::Coordinate;
 
 use super::sprites::{SPRITE_SCALE, SPRITE_SIZE};
@@ -6,10 +7,10 @@ pub const TILE_SIZE: f32 = SPRITE_SIZE * SPRITE_SCALE;
 pub const MAP_WIDTH: i32 = 100;
 pub const MAP_HEIGHT: i32 = 100;
 
-pub fn world_coordinate_from_grid(grid_coordinate: &Coordinate<i32>) -> Coordinate<f32> {
+pub fn world_coordinate_from_grid(grid_coordinate: &Coordinate<i32>) -> Vec2 {
     // conversion formula: tile_size * coordinate - (tile_size * (map_width / 2))
     let x = TILE_SIZE * grid_coordinate.x as f32 - (TILE_SIZE * (MAP_WIDTH / 2) as f32);
     let y = TILE_SIZE * grid_coordinate.y as f32 - (TILE_SIZE * (MAP_HEIGHT / 2) as f32);
 
-    return Coordinate { x, y };
+    return Vec2::new(x, y);
 }
