a4d1f165064a237a5c25a8d7870074370062ada8
Derrick Plotsky <derrickplotsky@gmail.com>
2021-11-01 22:35:29 -0700

    Add builder method for CropBundle
    Similar to some other bundles where I added a build method, I'm adding
    a small one for this.

--- src/components/crop.rs
+++ src/components/crop.rs
@@ -1,6 +1,15 @@
-use bevy::prelude::{Bundle, SpriteSheetBundle};
+use bevy::{
+    math::Vec3,
+    prelude::{Bundle, SpriteSheetBundle, Transform},
+    sprite::TextureAtlasSprite,
+};
 
-use super::name::Name;
+use crate::{
+    configuration::{crops::CropConfiguration, sprites::CROP_SPRITE_SCALE},
+    sprites::Sprites,
+};
+
+use super::{name::Name, spawns::CropSpawn};
 
 pub struct Crop {
     pub current_stage_index: usize,
@@ -26,4 +35,37 @@ pub struct CropBundle {
 
     #[bundle]
     pub sprite: SpriteSheetBundle,
+}
+
+impl CropBundle {
+    pub fn build(spawn: &CropSpawn, sprites: &Sprites, config: &CropConfiguration) -> Self {
+        let stages: Vec<CropStage> = config
+            .stages
+            .iter()
+            .map(|stage| CropStage {
+                ticks_in_stage: 0,
+                min_ticks_in_stage: stage.min_ticks_in_stage,
+                chance_to_advance: stage.chance_to_advance,
+                sprite_index: stage.sprite_index.unwrap(),
+            })
+            .collect();
+        Self {
+            sprite: SpriteSheetBundle {
+                transform: Transform {
+                    translation: Vec3::new(spawn.location.x, spawn.location.y, CROP_SPRITE_SCALE),
+                    scale: crate::configuration::sprites::sprite_scale(),
+                    ..Default::default()
+                },
+                sprite: TextureAtlasSprite::new(stages.get(0).unwrap().sprite_index),
+                texture_atlas: sprites.atlas_handle.clone(),
+                ..Default::default()
+            },
+            name: Name(config.name),
+            stages: CropStages { stages },
+            crop: Crop {
+                config_index: spawn.configuration_index,
+                current_stage_index: 0,
+            },
+        }
+    }
 }
--- src/systems/spawns.rs
+++ src/systems/spawns.rs
@@ -1,19 +1,8 @@
-use bevy::{
-    math::Vec3,
-    prelude::{Commands, Mut, Query, Res, SpriteSheetBundle, Transform},
-    sprite::TextureAtlasSprite,
-};
+use bevy::prelude::{Commands, Mut, Query, Res};
 
 use crate::{
-    components::{
-        crop::{Crop, CropBundle, CropStage, CropStages},
-        name::Name,
-        spawns::{CropSpawn, Spawns},
-    },
-    configuration::{
-        crops::{CropConfiguration, CropConfigurations},
-        sprites::CROP_SPRITE_SCALE,
-    },
+    components::{crop::CropBundle, spawns::Spawns},
+    configuration::crops::CropConfigurations,
     sprites::Sprites,
 };
 
@@ -41,38 +30,7 @@ pub fn spawn_crops(
             _ => continue,
         };
 
-        commands.spawn_bundle(crop_bundle(spawn, &sprites, config));
-    }
-}
-
-fn crop_bundle(spawn: &CropSpawn, sprites: &Sprites, config: &CropConfiguration) -> CropBundle {
-    let stages: Vec<CropStage> = config
-        .stages
-        .iter()
-        .map(|stage| CropStage {
-            ticks_in_stage: 0,
-            min_ticks_in_stage: stage.min_ticks_in_stage,
-            chance_to_advance: stage.chance_to_advance,
-            sprite_index: stage.sprite_index.unwrap(),
-        })
-        .collect();
-    CropBundle {
-        sprite: SpriteSheetBundle {
-            transform: Transform {
-                translation: Vec3::new(spawn.location.x, spawn.location.y, CROP_SPRITE_SCALE),
-                scale: crate::configuration::sprites::sprite_scale(),
-                ..Default::default()
-            },
-            sprite: TextureAtlasSprite::new(stages.get(0).unwrap().sprite_index),
-            texture_atlas: sprites.atlas_handle.clone(),
-            ..Default::default()
-        },
-        name: Name(config.name),
-        stages: CropStages { stages },
-        crop: Crop {
-            config_index: spawn.configuration_index,
-            current_stage_index: 0,
-        },
+        commands.spawn_bundle(CropBundle::build(spawn, &sprites, config));
     }
 }
 
